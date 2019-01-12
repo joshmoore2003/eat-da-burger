2dffedd4c993044ac193b17ba8ea5e692ad11d6c
Joshua Moore <jommoore2003@me.com>
2019-01-11 23:27:53 -0800

    Adding files and folder structure

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+DROP DATABASE burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+	id INTEGER auto_increment not null,
+    burger_name VARCHAR(50),
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP not null DEFAULT current_timestamp,
+    primary key(id)
+    );
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('Hawaiian Burger', false),
+    ('Double Bacon Burger', false),
+    ('Fat Bomb Burger', false),
+    ('Cheese Burger', true);
+    
+    select * from burgers;
